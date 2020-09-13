036fac621d2904c314af4ea676a2324464d3de05
HeySurfer <58826689+heysurfer@users.noreply.github.com>
2020-09-13 16:06:11 +0300

    Add files via upload

---
+++ serverdata/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proxy
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string a;
+            WebClient web = new WebClient();
+            System.IO.Stream stream = web.OpenRead("http://growtopia2.com/growtopia/server_data.php");
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(stream))
+            {
+                String text = reader.ReadToEnd();
+
+                a = text;
+
+            }
+            String texts = a;
+
+            texts = a;
+            texts = texts.Replace("server|", "");
+            texts = texts.Replace("port|", "");
+
+
+            string ip;
+            string port;
+
+            string ayir = texts;
+            string[] bosluk;
+            bosluk = ayir.Split();
+            ip = bosluk[0];
+            port = bosluk[2];
+            Console.WriteLine("Ip : " + ip);
+            Console.WriteLine("Port : " + port);
+            Console.WriteLine("Server Data Reader By HeySurfer");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ serverdata/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Ip Finder By HeySurfer")]
+[assembly: AssemblyDescription("Ip Finder By HeySurfer")]
+[assembly: AssemblyConfiguration("Ip Finder By HeySurfer")]
+[assembly: AssemblyCompany("Ip Finder By HeySurfer")]
+[assembly: AssemblyProduct("Ip Finder By HeySurfer")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("413e13c3-fd4d-4411-95b4-a48d0b80ce95")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
